1c5b9b0c23005b2e819dc49adaa07366f8066184
kristine bermoy <kbermoy02@gmail.com>
2023-11-30 21:52:21 -0800

    finished UI, started js implementation

---
+++ calculator/script.js
@@ -0,0 +1,13 @@
+// change the answer screen
+function getAnswer() {
+    document.querySelector('.answer').innerHTML = equation;
+}
+
+function reset() {
+    document.querySelector('.answer').innerHTML = '0';
+}
+
+function equation() {
+    // onclick add to string
+    
+}
